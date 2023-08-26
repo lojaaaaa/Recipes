8a61b9c2443b3dbb6ab55477dbbaae0811387c96
Alexander <a.ayugov@zebrains.team>
2023-08-26 18:12:06 +0400

    redesign recipe-card

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -7,10 +7,18 @@ const RecipeCard = ({image, title, desc, ingredients}) => {
       <div class="dishes__card-image">
         <img src={image} alt="card" class="dishes__card-img"/>
       </div>
-      <div div class="dishes__card-main">
-        <h4 class="dishes__card-title">{title}</h4>
-        <p class="dishes__card-desk">{desc}</p>
-        <p class="dishes__card-text">{ingredients.join(', ')}</p>
+      <div class="dishes__card-main">
+        <div class="dishes__main">
+          <div class="dishes__main-text">
+            <h4 class="dishes__card-title">{title}</h4>
+            <p class="dishes__card-desk">{desc}</p>
+            <p class="dishes__card-text">{ingredients.join(', ')}</p>
+          </div>
+        </div>
+        <div class="dishes__rating">
+          <button class="dishes__card-btn btn">Delete</button>
+          <button class="dishes__card-btn btn">Favorite</button>
+        </div>
       </div>
     </div>
   )
