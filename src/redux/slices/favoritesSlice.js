fe812825fdc3bb3a86d0de389c09649aad01cec1
Alexander <a.ayugov@zebrains.team>
2023-08-28 12:58:19 +0400

    fix some bugs in favoritesSlice

--- src/redux/slices/favoritesSlice.js
+++ src/redux/slices/favoritesSlice.js
@@ -1,16 +1,40 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 
+const parseFavorites =  () => {
+  const favorites =  JSON.parse(localStorage.getItem("favorites"))
+  return favorites || []
+}
+
+const setFavorites = (favorites, dispatch) => {
+  localStorage.setItem("favorites", JSON.stringify(favorites))
+  dispatch(updateFavorites(favorites))
+}
+
+
+export const getFromLocalStorage = createAsyncThunk(
+  'favorites/getFromLocalStorage',
+  async function(_, {rejectWithValue}){
+    try {
+      const favorites = await parseFavorites()
+      return favorites
+
+    } catch (error) {
+      return rejectWithValue
+    }
+  }
+)
+
+
 export const addToLocalStorage = createAsyncThunk(
   'favorites/addToLocalStorage',
   async function(favorite, {rejectWithValue, dispatch}){
     try {
-      let favorites = await JSON.parse(localStorage.getItem("favorites"))
+      const favorites = await parseFavorites()
 
       if (!favorites.find(f => f.id === favorite.id)){
         favorites.push(favorite)
-        await localStorage.setItem("favorites", JSON.stringify(favorites))
-        dispatch(updateFavorites(favorites))
+        await setFavorites(favorites, dispatch)
       }
       
     } catch (error) {
@@ -23,12 +47,8 @@ export const deleteFromLocalStorage = createAsyncThunk(
   'favorites/deleteFromLocalStorage',
   async function(favorite, {rejectWithValue, dispatch}){
     try {
-      let favorites = await JSON.parse(localStorage.getItem("favorites"))
-
-      favorites = favorites.filter(f => f.id !== favorite.id)
-      await localStorage.setItem("favorites", JSON.stringify(favorites))
-      dispatch(updateFavorites(favorites))
-
+      const favorites = await parseFavorites()
+      await setFavorites(favorites.filter(f => f.id !== favorite.id), dispatch)
       
     } catch (error) {
       return rejectWithValue
@@ -37,24 +57,14 @@ export const deleteFromLocalStorage = createAsyncThunk(
 )
 
 
-export const getFromLocalStorage = createAsyncThunk(
-  'favorites/getFromLocalStorage',
-  async function(_, {rejectWithValue}){
-    try {
-      const favorites = await JSON.parse(localStorage.getItem("favorites"));
-      return favorites
-
-    } catch (error) {
-      return rejectWithValue
-    }
-  }
-)
 
 
 const favoritesSlice = createSlice({
   name: 'favorites',     
   initialState: {               
     favorites: [],
+    status: 'init',
+    error: null
   },
   reducers: {
     updateFavorites(state, action){
@@ -62,9 +72,19 @@ const favoritesSlice = createSlice({
     },
   },
   extraReducers: {
+    [getFromLocalStorage.pending]: (state, action) => {
+      state.status = 'loading'
+      state.error = null;
+    },
     [getFromLocalStorage.fulfilled]: (state, action) => {
       state.favorites = action.payload;
-  },
+      state.status = 'success'
+    },
+    [getFromLocalStorage.rejected]: (state, action) => {
+      state.favorites = action.payload;
+      state.status = 'success'
+      state.error = action.payload
+    },
   }
   
   
